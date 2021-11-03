98497f8bf6e9a36a50c26cbc573b0b2fdab826d3
JosephKithome <jmulingwakithome.jmk@gmail.com>
2021-11-03 17:20:29 +0300

    deployment

--- app/app/settings.py
+++ app/app/settings.py
@@ -30,12 +30,12 @@ SECRET_KEY =  os.environ.get("SECRET_KEY", 'changeme')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
-ALLOWED_HOSTS_ENV=os.environ.get("ALLOWED_HOSTS")
+# ALLOWED_HOSTS_ENV=os.environ.get("ALLOWED_HOSTS")
 
-if ALLOWED_HOSTS_ENV:
-    ALLOWED_HOSTS.extend(ALLOWED_HOSTS_ENV.split(','))
+# if ALLOWED_HOSTS_ENV:
+#     ALLOWED_HOSTS.extend(ALLOWED_HOSTS_ENV.split(','))
 
 
 # Application definition
