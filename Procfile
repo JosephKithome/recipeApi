347c3ae67d75f613db41fbb1d17bf3881ed7e9b3
JosephKithome <jmulingwakithome.jmk@gmail.com>
2021-11-03 13:31:46 +0300

    added procfile

---
+++ Procfile
@@ -0,0 +1,5 @@
+release: python manage.py makemigrations
+release: python manage.py migrate
+release: python manage.py collectstatic --noinput
+
+web: gunicorn app.wsgi
--- app/app/settings.py
+++ app/app/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 
+# Configure Django App for Heroku.
+import django_heroku
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -146,3 +150,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 AUTH_USER_MODEL = "core.User"
+
+django_heroku.settings(locals())
